1c73edaea91152333e20aaaf4a025f036453b48a
anuragparida <anuragparida37@gmail.com>
2020-10-04 00:23:53 +0530

    Fixed Initial

--- routes/redirect.js
+++ routes/redirect.js
@@ -4,13 +4,13 @@ const redirectControllers = require("../controllers/redirect");
 
 const router = express.Router();
 
-router.get("/", () => {
-  if (1 === 1) {
-    return redirectControllers.getRedirect;
+router.get("/", (req, res, next) => {
+  if (req.originalUrl.slice(1) === "sasa") {
+    res.redirect("https://www.google.com");
   } else {
+    next("router");
   }
 });
-// TODO: redirect to set website
 //if not found then next('route')
 
 module.exports = router;
